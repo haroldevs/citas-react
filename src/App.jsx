02c16379f9a37b928e9b982e2d2fe1bb678a9452
haroldevs <haroldthomasg18@gmail.com>
2023-03-21 20:33:43 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
+import { useState } from "react";
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState([]);
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex ">
+        <Formulario setPacientes={setPacientes} pacientes={pacientes} paciente={paciente} />
+        <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
